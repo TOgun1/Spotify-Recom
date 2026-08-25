202afb9c2b242a592a6e0269f468894c91c7df61
TOgun1 <tobiogunlana@outlook.com>
2026-08-25 21:35:23 +0100

    Dataframe established and artist ids retrieved

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from starlette.middleware.sessions import SessionMiddleware
-from spotify_client import get_authorize_url, get_access_token, get_spotify_client
+from spotify_client import get_artists_details, get_authorize_url, get_access_token,get_artists_details, get_spotify_client,get_top_tracks, get_recently_played, get_related_artists, get_artist_top_tracks
 import os
+from recommender import build_lookup_dict, create_dataframe
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -23,14 +24,49 @@ def login():
 def callback(request: Request, code: str):
     token = get_access_token(code)
     request.session["access_token"] = token
-    return {"access_token": "success"}
+    return RedirectResponse(url="/recommendations")
 
 @app.get("/recommendations")
 def recommendations(request: Request):
     token = request.session.get("access_token")
     sp = get_spotify_client(token)
+    parsed_data = []
 
     top_tracks = get_top_tracks(sp)
+    tracks_data = top_tracks['items']
+    for t in tracks_data:
+        track_id = t['id']
+        track_name = t['name']
+        artist_ids = [artist['id'] for artist in t['artists']]
+        artist_names = [artist['name'] for artist in t['artists']]
+        popularity = t.get('popularity', 0)
+        
+        parsed_data.append((track_id, track_name, artist_ids, artist_names, popularity))
+
     recently_played = get_recently_played(sp)
-    #To be continued once the recommendation algorithm is implemented
-    
+    for item in recently_played['items']:
+        t = item.get('track', {})
+        track_id = t.get('id')
+
+        if not track_id:
+            continue
+
+        track_name = t.get('name')
+        artist_ids = [artist['id'] for artist in t.get('artists', [])]
+        artist_names = [artist['name'] for artist in t.get('artists', [])]
+        popularity = t.get('popularity', 0)
+
+        parsed_data.append((track_id, track_name, artist_ids, artist_names, popularity))
+
+    df = create_dataframe(parsed_data)
+    unique_artists_ids = {artist_id for artist_list in df['artist_ids'] for artist_id in artist_list}
+    batch_size = 50
+    artist_details = []
+
+    for i in range(0, len(unique_artists_ids), batch_size):
+        batch = unique_artists_ids[i:i + batch_size]
+        result = get_artists_details(sp, batch)
+        artist_details.extend(result['artists'])
+
+    table = build_lookup_dict(artist_details)
+    return {"top_tracks": parsed_data}
--- backend/recommender.py
+++ backend/recommender.py
@@ -2,3 +2,16 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
+def create_dataframe(tracks_data):
+    df = pd.DataFrame(tracks_data, columns=['track_id', 'track_name', 'artist_ids', 'artist_names', 'popularity']).drop_duplicates(subset='track_id')
+    return df
+
+def build_lookup_dict(details):
+    info = {}
+    for artist in details:
+        info[artist['id']] = {
+            'genres': artist.get('genres', []),
+            'followers': artist['followers']['total'],
+        }
+    return info
