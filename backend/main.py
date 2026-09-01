6aefbae0a884d454df5d6e54e9fa571aaf45f435
TOgun1 <tobiogunlana@outlook.com>
2026-09-01 15:44:44 +0100

    Separated Recommendation logic from main to improve modularity

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import RedirectResponse
 from starlette.middleware.sessions import SessionMiddleware
 from spotify_client import get_artists_details, get_authorize_url, get_access_token,get_artists_details, get_spotify_client,get_top_tracks, get_recently_played, get_related_artists, get_artist_top_tracks
 import os
-from recommender import build_lookup_dict, create_dataframe, get_genres_for_row, get_min_followers_for_row
+from recommender import build_lookup_dict, create_dataframe, extract_recently_played_data, extract_track_data, get_genres_for_row, get_min_followers_for_row, separate_into_unique_artists
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -34,41 +34,18 @@ def recommendations(request: Request):
 
     top_tracks = get_top_tracks(sp)
     tracks_data = top_tracks['items']
-    for t in tracks_data:
-        track_id = t['id']
-        track_name = t['name']
-        artist_ids = [artist['id'] for artist in t['artists']]
-        artist_names = [artist['name'] for artist in t['artists']]
-        popularity = t.get('popularity', 0)
-        
-        parsed_data.append((track_id, track_name, artist_ids, artist_names, popularity))
+    extract_track_data(tracks_data, parsed_data)
 
     recently_played = get_recently_played(sp)
-    for item in recently_played['items']:
-        t = item.get('track', {})
-        track_id = t.get('id')
-
-        if not track_id:
-            continue
-
-        track_name = t.get('name')
-        artist_ids = [artist['id'] for artist in t.get('artists', [])]
-        artist_names = [artist['name'] for artist in t.get('artists', [])]
-        popularity = t.get('popularity', 0)
-
-        parsed_data.append((track_id, track_name, artist_ids, artist_names, popularity))
+    extract_recently_played_data(recently_played, parsed_data)
 
     df = create_dataframe(parsed_data)
-    unique_artists_ids = {artist_id for artist_list in df['artist_ids'] for artist_id in artist_list}
-    batch_size = 50
-    artist_details = []
-
-    for i in range(0, len(unique_artists_ids), batch_size):
-        batch = unique_artists_ids[i:i + batch_size]
-        result = get_artists_details(sp, batch)
-        artist_details.extend(result['artists'])
+    artist_details = separate_into_unique_artists(df)
 
     table = build_lookup_dict(artist_details)
     df['genres'] = df['artist_ids'].apply(lambda x: get_genres_for_row(x, table))
     df['min_followers'] = df['artist_ids'].apply(lambda x: get_min_followers_for_row(x, table))
+
+    #Build Candidate Pool
+    
     return {"top_tracks": df}
--- backend/recommender.py
+++ backend/recommender.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from backend.spotify_client import get_related_artists, get_artists_details
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -16,6 +17,42 @@ def build_lookup_dict(details):
         }
     return info
 
+def extract_track_data(tracks_data,parsed_data):
+    for t in tracks_data:
+            track_id = t['id']
+            track_name = t['name']
+            artist_ids = [artist['id'] for artist in t['artists']]
+            artist_names = [artist['name'] for artist in t['artists']]
+            popularity = t.get('popularity', 0)
+            
+            parsed_data.append((track_id, track_name, artist_ids, artist_names, popularity))
+    return parsed_data
+
+def extract_recently_played_data(recently_played, parsed_data):
+    for item in recently_played['items']:
+            t = item.get('track', {})
+            track_id = t.get('id')
+    
+            if not track_id:
+                continue
+    
+            track_name = t.get('name')
+            artist_ids = [artist['id'] for artist in t.get('artists', [])]
+            artist_names = [artist['name'] for artist in t.get('artists', [])]
+            popularity = t.get('popularity', 0)
+    
+            parsed_data.append((track_id, track_name, artist_ids, artist_names, popularity))
+
+def separate_into_unique_artists(df):
+     unique_artists_ids = {artist_id for artist_list in df['artist_ids'] for artist_id in artist_list}
+     batch_size = 50
+     artist_details = []
+     for i in range(0, len(unique_artists_ids), batch_size):
+         batch = unique_artists_ids[i:i + batch_size]
+         result = get_artists_details(sp, batch)
+         artist_details.extend(result['artists'])
+     return artist_details
+
 def get_genres_for_row(artist_ids, info):
     genres = set()
     for artist_id in artist_ids:
@@ -27,3 +64,13 @@ def get_min_followers_for_row(artist_ids, info):
     if not follower_counts:
         return 0
     return min(follower_counts)
+
+def construct_related_artists_df(df, sp, artist_ids):
+    graph = {}
+    for artist_id in artist_ids:
+        related_artists = get_related_artists(sp, artist_id)
+
+        for artist in related_artists['artists']:
+            top_tracks = sp.artist_top_tracks(sp,artist['id'])
+            tracks = top_tracks['tracks']
+            unique_tracks = [track for track in tracks if track['id'] not in df['track_id'].values]
